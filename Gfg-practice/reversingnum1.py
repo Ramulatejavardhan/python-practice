5299d5a79330b941a2bde712d950aeb1aea862a0
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-06-05 14:30:54 +0530

    reversenum

--- Gfg-practice/reversingnum1.py
+++ Gfg-practice/reversingnum1.py
@@ -2,7 +2,7 @@
 n=int(input())
 s=0
 while n>0:
-    a=n%10
-    s=s*10+a
-    n=n//10
+    a=n%10 #it gives the ending number of input. 
+    s=s*10+a #multiplies the s with 10 and add the end value of the input to it.
+    n=n//10 #removes the ending number from input.
 print(s)
