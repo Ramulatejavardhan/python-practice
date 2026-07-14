f7afb77fde66f66c98cabab64b6ace9dfb349ff1
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-14 10:19:25 +0530

    filewriting

--- python-filehandling/filedetection.py
+++ python-filehandling/filedetection.py
@@ -1,6 +1,16 @@
 import os
-file_path="examp.txt"
-if os.path.exists(file_path):
-    print("file exists")
-else:
-    print("not found")
+#               |_relative path_|
+# file_path="examp.txt"
+# if os.path.exists(file_path):
+#     print("file exists")
+# else:
+#     print("not found")
+
+
+file="name/text.txt"
+if os.path.exists(file):
+    print("exists")
+
+files="C:/Users/user/Desktop/tests"
+if os.path.exists(files):
+    print("absoulte file found")
