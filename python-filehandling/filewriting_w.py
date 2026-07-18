745093b44f5fbc3cfe101e3c8dda29dcb9f29a62
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-18 12:28:53 +0530

    upd

--- python-filehandling/filewriting_w.py
+++ python-filehandling/filewriting_w.py
@@ -1,5 +1,5 @@
-txt="hello created this file with filewriting"
-file="example.txt"
+txt="hello created this file with filewritings"
+file="examples.txt"
 with open(file,"w") as file:#"w" mode is used to create a file and write the data on file.
     file.write(txt)
-    print("added the text to example.txt")
+    print("added the text to examp.txt")
